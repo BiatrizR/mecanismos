01f0f6bfff41171013a8ba4d5d9ab375f7639182
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-01-12 19:32:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//importar os módulos de mecanismos de fisica
+const Engine = Matter.Engine;
+const World =
+
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+     
+
+ 
+}
+
+
+function draw() 
+{
+  background(51);
+
+  
+  
+}
+
